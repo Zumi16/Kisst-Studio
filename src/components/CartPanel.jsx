847b9b2fd575198ba47c33f7f7e2ac9d8ad68a16
Zumi16 <mizuzumi16@gmail.com>
2026-09-01 20:12:24 +0800

    Implement CartPanel component with backdrop and close functionality; update Header to manage cart state

--- src/components/CartPanel.jsx
+++ src/components/CartPanel.jsx
@@ -1,5 +1,17 @@
-export function CartPanell() {
+import './CartPanel.css'
+
+export function CartPanel({ isCartOpen, onClose }) {
     return (
-        <div>Cart ito</div>
+        <>
+        <div className={`panel-backdrop ${isCartOpen ? 'open' : ''}`}></div>
+            <div className={`cart-panel ${isCartOpen ? 'open' : ''}`}>
+                <div className='cart-header'>
+                    <button className='close-panel' onClick={onClose}>Close</button>
+                    <h1>What Is this</h1>
+                </div>
+            </div>
+        </>
     )
 }
+
+export default CartPanel;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './Header.css'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import CartPanel from './CartPanel';
 
 export function Header({ products, total }) {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
+    function openCartPanel() {
+        setIsCartOpen(true)
+    }
 
     return (
         <header>
@@ -9,7 +16,7 @@ export function Header({ products, total }) {
                 <div className='left-section'>
                     <img className='logo' src='images/logo-white.png' />
                 </div>
-                <div className='right-section'>
+                <div className='right-section' onClick={openCartPanel}>
                     <ShoppingCartOutlinedIcon fontSize='large' />
                     <p>Cart</p>
                 </div>
@@ -23,7 +30,7 @@ export function Header({ products, total }) {
                     <p>Clear All</p>
                 </div>
                 <div className='right-section'>
-                    <input className='search-bar' placeholder='Search'/>
+                    <input className='search-bar' placeholder='Search' />
                 </div>
             </div>
             <div className='result-header'>
@@ -33,6 +40,8 @@ export function Header({ products, total }) {
                     <p>Category 2</p>
                 </div>
             </div>
+            
+            <CartPanel isCartOpen={isCartOpen} onClose={() => setIsCartOpen(false)}/>
         </header>
     )
 }
