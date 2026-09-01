fa393ec9099f54b866dd55a99b5c7bd81e918cb4
Zumi16 <mizuzumi16@gmail.com>
2026-09-01 22:24:03 +0800

    Enhance CartPanel component: update layout, add cart item display, and improve styling for better usability

--- src/components/CartPanel.jsx
+++ src/components/CartPanel.jsx
@@ -3,11 +3,31 @@ import './CartPanel.css'
 export function CartPanel({ isCartOpen, onClose }) {
     return (
         <>
-        <div className={`panel-backdrop ${isCartOpen ? 'open' : ''}`}></div>
+            <div className={`panel-backdrop ${isCartOpen ? 'open' : ''}`}></div>
             <div className={`cart-panel ${isCartOpen ? 'open' : ''}`}>
                 <div className='cart-header'>
                     <button className='close-panel' onClick={onClose}>Close</button>
-                    <h1>What Is this</h1>
+                    <h2>Your Cart (3 Items)</h2>
+                </div>
+                <div className='cart-body'>
+                    <div className='cart-container'>
+                        {/* Cart items sample */}
+                        <div className='cart-item'>
+                            <div className='sample-img'>sample image</div>
+                            <div className='item-detail'>
+                                <h3>Item Titlefggggggggggg</h3>
+                                <p>Price $9.28</p>
+                                <p>item - 1 +</p>
+                            </div>
+                            <button className='remove-item'>Remove</button>
+                        </div>
+                    </div>
+                    <div className='total-container'>
+                        <p>Subtotal (before discount) $33.97</p>
+                        <p>You save -$1.69</p>
+                        <p>Total $32.28</p>
+                        <button>Clear cart</button>
+                    </div>
                 </div>
             </div>
         </>
