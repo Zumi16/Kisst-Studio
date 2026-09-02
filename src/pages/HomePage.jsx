05b0590981af49e9ca2d992c726ca0726361cece
Zumi16 <mizuzumi16@gmail.com>
2026-09-02 21:53:01 +0800

    Enhance ProductGrid and HomePage components: add cart functionality, improve product display, and style add to cart button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
 import { Header } from "../components/Header";
 import './HomePage.css'
 import ProductGrid from "./ProductGrid";
-
+import { useState } from "react";
 
 function HomePage({products, total, selectedId,setSelected}) {
+    const [cart, setCart] = useState([]);
+
     return (
         <>
             <Header products={products} total={total}/>
             <div className="home-page">
-                <ProductGrid products={products} selectedId={selectedId} setSelected={setSelected}/>
+                <ProductGrid products={products} selectedId={selectedId} setSelected={setSelected} setCart={setCart}/>
             </div>
+            {console.log(cart)}
         </>
     )
 }
--- src/pages/ProductGrid.jsx
+++ src/pages/ProductGrid.jsx
@@ -1,26 +1,29 @@
 import Modal from "../components/Modal";
 
-export function ProductGrid({ products, selectedId, setSelected }) {
+export function ProductGrid({ products, selectedId, setSelected, setCart}) {
+
     return (
         <div className="product-grid">
             {products.map((product) => {
                 return (
                     <div className="product" key={product.id} onClick={() => setSelected(product)}>
-                        <img src={product.thumbnail} />
-                        <h1>{product.title}</h1>
                         {/* <p>Description: {product.description}</p> */}
-                        <p>Category: {product.category}</p>
-                        <p>Rating: {product.rating}</p>
-                        <p>Price: {product.price}</p>
-                        <p>Stock: {product.stock}</p>
-                        <p>Add to cart</p>
+                        <div className="product-detail">
+                            <img src={product.thumbnail} />
+                            <h1>{product.title}</h1>
+                            <p>Category: {product.category}</p>
+                            <p>Rating: {product.rating}</p>
+                            <p>Price: {product.price}</p>
+                            <p>Stock: {product.stock}</p>
+                        </div>
+                        <button className="addtocart-btn" onClick={(e) => {e.stopPropagation(); setCart(cartItems => [...cartItems, product.id]);}}>Add to cart</button>
                     </div>
                 )
             })}
 
             {selectedId && (
-                <Modal 
-                    selectedId={selectedId} 
+                <Modal
+                    selectedId={selectedId}
                     setSelected={setSelected}
                 />
             )}
